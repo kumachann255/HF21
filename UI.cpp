4de59ca3e62c75b6e64defcd8f380757e6b3f96f
DESKTOP-E8FR2SJ\DELL <hal.kashiwamura.daichi.2021@gmail.com>
2022-11-30 20:27:40 +0900

    UI生成クラスできました。

--- UI.cpp
+++ UI.cpp
@@ -40,13 +40,15 @@ UITexData::UITexData()
 //=============================================================================
 UITexData::UITexData(char *textureName) : m_TextureName(textureName)
 {
+	//Create();
 }
 
 //=============================================================================
 // テクスチャー生成処理
 //=============================================================================
-HRESULT UITexData::Create(void)
+HRESULT UITexData::Create(char *textureName)
 {
+	m_TextureName = textureName;
 
 	// テクスチャ生成
 	D3DX11CreateShaderResourceViewFromFile(GetDevice(),
@@ -104,11 +106,9 @@ UITexData::~UITexData()
 //=============================================================================
 UIObject::UIObject()
 {
-
 	m_UITexData = new UITexData;
+	m_isUse = TRUE;
 }
-
-
 
 //=============================================================================
 // デストラクター
@@ -123,7 +123,12 @@ UIObject::~UIObject()
 //=============================================================================
 void UIObject::Update(void)
 {
+	m_timeCnt++;
 
+	if (m_timeCnt > m_timeLimit)
+	{
+		m_isUse = FALSE;
+	}
 }
 
 //=============================================================================
@@ -132,49 +137,53 @@ void UIObject::Update(void)
 void UIObject::Draw(void)
 {
 
-	//// 2Dの物を描画する処理
-	//// Z比較なし
-	//SetDepthEnable(FALSE);
+	if (!m_isUse) return;
 
-	//// ライティングを無効
-	//SetLightEnable(FALSE);
+	SetViewPort(TYPE_FULL_SCREEN);
 
-	//// 頂点バッファ設定
-	//UINT stride = sizeof(VERTEX_3D);
-	//UINT offset = 0;
-	//GetDeviceContext()->IASetVertexBuffers(0, 1, m_UITexData->GetpVertexBuffer(), &stride, &offset);
+	// 2Dの物を描画する処理
+	// Z比較なし
+	SetDepthEnable(FALSE);
 
-	//// マトリクス設定
-	//SetWorldViewProjection2D();
+	// ライティングを無効
+	SetLightEnable(FALSE);
 
-	//// プリミティブトポロジ設定
-	//GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	// 頂点バッファ設定
+	UINT stride = sizeof(VERTEX_3D);
+	UINT offset = 0;
+	GetDeviceContext()->IASetVertexBuffers(0, 1, m_UITexData->GetpVertexBuffer(), &stride, &offset);
 
-	//// マテリアル設定
-	//MATERIAL material;
-	//ZeroMemory(&material, sizeof(material));
-	//material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	//SetMaterial(material);
+	// マトリクス設定
+	SetWorldViewProjection2D();
 
-	//// タイトルの背景を描画
-	//{
-	//	// テクスチャ設定
-	//	GetDeviceContext()->PSSetShaderResources(0, 1, m_UITexData->GetTexture());
+	// プリミティブトポロジ設定
+	GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
-	//	// １枚のポリゴンの頂点とテクスチャ座標を設定
-	//	SetSpriteColorRotation(m_UITexData->GetVertexBuffer(),
-	//		m_pos.x, m_pos.y, m_fWidth, m_fHeight,
-	//		0.0f, 0.0f, 1.0f, 1.0f,
-	//		m_color, m_rot);
+	// マテリアル設定
+	MATERIAL material;
+	ZeroMemory(&material, sizeof(material));
+	material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	SetMaterial(material);
 
-	//	// ポリゴン描画
-	//	GetDeviceContext()->Draw(4, 0);
-	//}
+	// タイトルの背景を描画
+	{
+		// テクスチャ設定
+		GetDeviceContext()->PSSetShaderResources(0, 1, m_UITexData->GetTexture());
 
-	//// ライティングを有効に
-	//SetLightEnable(TRUE);
+		// １枚のポリゴンの頂点とテクスチャ座標を設定
+		SetSpriteColorRotation(m_UITexData->GetVertexBuffer(),
+			m_pos.x, m_pos.y, m_fWidth, m_fHeight,
+			0.0f, 0.0f, 1.0f, 1.0f,
+			m_color, m_rot);
 
-	//// Z比較あり
-	//SetDepthEnable(TRUE);
+		// ポリゴン描画
+		GetDeviceContext()->Draw(4, 0);
+	}
+
+	// ライティングを有効に
+	SetLightEnable(TRUE);
+
+	// Z比較あり
+	SetDepthEnable(TRUE);
 }
 
